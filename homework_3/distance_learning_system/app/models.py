ad701539442b51d8a911bb1257f26e948a6b0e32
Anton <anton.komissarov.02@mail.ru>
2024-04-12 00:04:07 +0300

    Create models and make migration

--- homework_3/distance_learning_system/app/models.py
+++ homework_3/distance_learning_system/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    uid = models.UUIDField(verbose_name='ID курса')
+    name = models.CharField(max_length=100, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание курса')
+    paid = models.BooleanField(verbose_name='Платный курс')
+
+    def __str__(self):
+        return f'{self.name} [{self.uid}]'
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    uid = models.UUIDField(verbose_name='ID занятия')
+    theme = models.CharField(max_length=150, verbose_name='Тема занятия')
+    material = models.TextField(verbose_name='Материал занятия')
+    time = models.IntegerField(verbose_name='Продолжительность занятия (мин)')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return f'{self.theme} [{self.uid}]'
+
+    class Meta:
+        verbose_name = "Занятие"
+        verbose_name_plural = "Занятия"
