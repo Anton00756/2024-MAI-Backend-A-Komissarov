930438356a83848938ea46062002e109a22bb09d
Anton <anton.komissarov.02@mail.ru>
2024-04-26 00:58:10 +0300

    Complete JSON views

--- homework_3/distance_learning_system/app/views.py
+++ homework_3/distance_learning_system/app/views.py
@@ -1,67 +1,94 @@
+import json
+import uuid
+
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.views.decorators.http import require_http_methods
+from .models import Course, Lesson
+
+from django.views.decorators.csrf import csrf_exempt
 
 
 @require_http_methods(["GET"])
 def all_courses(request):
-    courses = [
-        {
-            'id': '4381462f-54f4-4f7e-af32-641bde5d1ef8',
-            'name': 'Курс №1',
-            'description': 'Описание курса №1'
-        },
-        {
-            'id': '93394e62-5511-4de9-ab93-1cbab1320254',
-            'name': 'Курс №2',
-            'description': 'Описание курса №2'
-        },
-    ]
-    return JsonResponse(courses, safe=False, json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(list(Course.objects.all().values()), safe=False, json_dumps_params={'ensure_ascii': False})
 
 
-@require_http_methods(["GET", "POST"])
+@require_http_methods(["GET"])
 def course(request, course_id):
-    if request.method == "GET":
-        courses = {
-            '4381462f-54f4-4f7e-af32-641bde5d1ef8': {
-                'name': 'Курс №1',
-                'description': 'Описание курса №1',
-                'lessons': [
-                    '4381462f-54f4-4f7e-af32-641bde5d1ef1',
-                    '4381462f-54f4-4f7e-af32-641bde5d1ef2',
-                    '4381462f-54f4-4f7e-af32-641bde5d1ef3',
-                ]
-            },
-            '93394e62-5511-4de9-ab93-1cbab1320254': {
-                'name': 'Курс №2',
-                'description': 'Описание курса №2',
-                'lessons': [
-                    '93394e62-5511-4de9-ab93-1cbab1320251',
-                    '93394e62-5511-4de9-ab93-1cbab1320252',
-                    '93394e62-5511-4de9-ab93-1cbab1320253',
-                ]
-            }
+    course_item = Course.objects.get(uid=course_id)
+    response_data = {
+        'uid': course_item.uid,
+        'name': course_item.name,
+        'description': course_item.description,
+        'paid': course_item.paid
+    }
+    return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@require_http_methods(["GET"])
+def search_course(request):
+    query = request.GET.get('q', '')
+    courses = Course.objects.filter(name__icontains=query) | Course.objects.filter(description__icontains=query)
+    response_data = {
+        'courses': list(courses.values())
+    }
+    return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def create_course(request):
+    try:
+        data = json.loads(request.body)
+        uid = uuid.uuid4()
+        course_item = Course.objects.create(uid=uid, name=data['name'], description=data['description'],
+                                            paid=data['paid'])
+
+        response_data = {
+            'uid': course_item.uid,
+            'name': course_item.name,
+            'description': course_item.description,
+            'paid': course_item.paid
         }
-        if (course_id := str(course_id)) not in courses:
-            return HttpResponseNotFound('Unknown course!')
-        return JsonResponse(courses[course_id], safe=False, json_dumps_params={'ensure_ascii': False})
-    else:
-        return HttpResponseBadRequest('NOT IMPLEMENTED')
+        return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False}, status=201)
+    except Exception as error:
+        return JsonResponse({'error': str(error)}, status=400)
 
 
-@require_http_methods(["GET", "POST"])
+@require_http_methods(["GET"])
+def all_lessons(request):
+    return JsonResponse(list(Lesson.objects.all().values()), safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@require_http_methods(["GET"])
 def lesson(request, lesson_id):
-    if request.method == "GET":
-        lessons = {
-            '4381462f-54f4-4f7e-af32-641bde5d1ef1': 'Текст занятия №1',
-            '4381462f-54f4-4f7e-af32-641bde5d1ef2': 'Текст занятия №2',
-            '4381462f-54f4-4f7e-af32-641bde5d1ef3': 'Текст занятия №3',
-            '93394e62-5511-4de9-ab93-1cbab1320251': 'Текст занятия №4',
-            '93394e62-5511-4de9-ab93-1cbab1320252': 'Текст занятия №5',
-            '93394e62-5511-4de9-ab93-1cbab1320253': 'Текст занятия №6',
+    lesson_item = Lesson.objects.get(uid=lesson_id)
+    response_data = {
+        'uid': lesson_item.uid,
+        'theme': lesson_item.theme,
+        'material': lesson_item.material,
+        'time': lesson_item.time,
+        'course_uid': lesson_item.course.uid
+    }
+    return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def create_lesson(request):
+    try:
+        data = json.loads(request.body)
+        uid = uuid.uuid4()
+        lesson_item = Lesson.objects.create(uid=uid, theme=data['theme'], material=data['material'], time=data['time'],
+                                            course=Course.objects.get(uid=data['course_uid']))
+
+        response_data = {
+            'uid': lesson_item.uid,
+            'theme': lesson_item.theme,
+            'material': lesson_item.material,
+            'time': lesson_item.time,
+            'course_uid': lesson_item.course.uid
         }
-        if (lesson_id := str(lesson_id)) not in lessons:
-            return HttpResponseNotFound('Unknown lesson!')
-        return JsonResponse(lessons[lesson_id], safe=False, json_dumps_params={'ensure_ascii': False})
-    else:
-        return HttpResponseBadRequest('NOT IMPLEMENTED')
+        return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False}, status=201)
+    except Exception as error:
+        return JsonResponse({'error': str(error)}, status=400)
